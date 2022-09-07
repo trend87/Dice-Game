c97bff6d98ee00a08fe56a7ff824f5ed371cc7d0
Effiong Israel Robert <israelrobert@gmail.com>
2022-09-07 23:53:17 +0100

    manipulated html using the querySelector to display random images on the class .img1 and .img2

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ let randomImage = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "
 let randomNumber1 = Math.floor((Math.random() * randomImage.length) )
 let randomNumber2 = Math.floor((Math.random() * randomImage.length) )
 
+document.querySelector(".img1").src = randomImage[randomNumber1]
+document.querySelector(".img2").src = randomImage[randomNumber2]
