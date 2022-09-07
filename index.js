2b426853004d40f886dd1a94366a11b3e7e1c363
Effiong Israel Robert <israelrobert@gmail.com>
2022-09-08 00:13:23 +0100

    created an if and else if condition inside the function Refresh() to determine status of player

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-let randomImage = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"]
-let randomNumber1 = Math.floor((Math.random() * randomImage.length) )
-let randomNumber2 = Math.floor((Math.random() * randomImage.length) )
+function Refresh(){
+    let randomImage = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"]
+    let randomNumber1 = Math.floor((Math.random() * randomImage.length) )
+    let randomNumber2 = Math.floor((Math.random() * randomImage.length) )
+    
+    document.querySelector(".img1").src = randomImage[randomNumber1]
+    document.querySelector(".img2").src = randomImage[randomNumber2]
+    
+    if(randomNumber1 < randomNumber2){
+        document.getElementById("title").textContent = "Player 2 Wins"
+    } else if(randomNumber1 === randomNumber2){
+        document.getElementById("title").textContent = "It's a Tie "
+    
+    }else if(randomNumber1 > randomNumber2){
+        document.getElementById("title").textContent = "Player 1 Wins"
+    }
 
-document.querySelector(".img1").src = randomImage[randomNumber1]
-document.querySelector(".img2").src = randomImage[randomNumber2]
+    document.getElementById("btn").textContent = "Play Again"
+
+}
