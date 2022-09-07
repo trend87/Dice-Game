1d1919e2cd4bcc0886c281fdf04bac94ca6b9368
Effiong Israel Robert <israelrobert@gmail.com>
2022-09-07 23:47:49 +0100

    modified html

---
+++ index.js
@@ -0,0 +1,5 @@
+let randomImage = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"]
+
+let randomNumber1 = Math.floor((Math.random() * randomImage.length) )
+let randomNumber2 = Math.floor((Math.random() * randomImage.length) )
+
